a8a20361e5f300221979de56d1c24343174f13be
MengLinMaker <lm8100mystery@gmail.com>
2022-08-11 20:28:53 +1000

    Modify for vite build and live demo

---
+++ js/3Dvisualiser.js
@@ -0,0 +1,227 @@
+import '../css/style.css'
+
+const sceneHeight = 2.6;
+
+
+
+
+
+// Setting up three js canvas
+import * as THREE from 'three';
+const scene = new THREE.Scene();
+scene.background = new THREE.Color( 0xffffff );
+//scene.fog = new THREE.Fog( 0xffffff, 20, 60 );
+
+const camera = new THREE.PerspectiveCamera(30, window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.up.set( 0, 0, 1 );
+scene.position.set(0,0,sceneHeight);
+camera.position.set( 24, 12, 12 );
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
+renderer.shadowMap.enabled = true;
+
+
+
+
+
+// Autosize canvas
+let canvas = document.getElementById('bg');
+window.addEventListener('resize', onWindowResize, false)
+onWindowResize()
+function onWindowResize() {
+  const container = document.getElementById('container');
+  camera.aspect = container.clientWidth/ container.clientHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( container.clientWidth, container.clientHeight );
+  renderer.render(scene, camera);
+}
+
+
+
+
+
+// Importing orbit controls
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+const controls = new OrbitControls(camera, renderer.domElement);
+
+
+
+
+
+
+// Defining lights
+scene.add( new THREE.HemisphereLight( 0x443333, 0x111122 ) );
+addShadowedLight( 10, 10, 50 + sceneHeight, 0xffffff, 2.5 );
+addShadowedLight( 50, 0, - 50 + sceneHeight, 0xffccaa, 3 );
+addShadowedLight( -10, -5, -10 + sceneHeight, 0xccaa88, 3 );
+function addShadowedLight( x, y, z, color, intensity ) {
+  const directionalLight = new THREE.DirectionalLight( color, intensity );
+  directionalLight.position.set( x, y, z );
+  scene.add( directionalLight );
+  directionalLight.castShadow = true;
+  const side = 10;
+  directionalLight.shadow.camera.top = side;
+  directionalLight.shadow.camera.bottom = -side;
+  directionalLight.shadow.camera.left = side;
+  directionalLight.shadow.camera.right = -side;
+}
+
+
+
+
+
+// Ground plane
+//*/
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry( 400, 400 ),
+    new THREE.MeshPhysicalMaterial( { color: 0xfffffff, specular: 0xffffff } )
+);
+plane.translateZ(-9);
+scene.add( plane );
+plane.receiveShadow = true;
+//*/
+
+
+
+
+
+
+// Defining geometries
+import { STLLoader } from 'three/examples/jsm/loaders/STLLoader'
+const loader = new STLLoader();
+const material = new THREE.MeshPhongMaterial( { color: 0xaa8866, specular: 0x111111, shininess: 50 } );
+
+let hip;
+import hipSTL from '../asset/hip.stl?url'
+loader.load(
+  hipSTL,
+  function (geometry) {
+    geometry.center();
+    const mesh = new THREE.Mesh( geometry, material );
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+
+    mesh.scale.set( -0.01, 0.01, 0.01 );
+    hip = mesh;
+    scene.add(mesh);
+
+    loadRightThigh();
+    loadLeftThigh();
+  }
+)
+
+let rightThigh;
+import rightThighSTL from '../asset/rightThigh.stl?url'
+function loadRightThigh(){
+  loader.load(
+    rightThighSTL,
+    function (geometry) {
+      geometry.center();
+      const mesh = new THREE.Mesh( geometry, material );
+      mesh.castShadow = true;
+      mesh.receiveShadow = true;
+
+      geometry.translate( 0, 20, -215 );
+      mesh.position.set( -10, -80, -10 );
+      mesh.scale.set( 1, -1, 1 );
+      rightThigh = mesh;
+      hip.add(rightThigh);
+    }
+  )
+}
+
+let leftThigh;
+import leftThighSTL from '../asset/rightThigh.stl?url'
+function loadLeftThigh(){
+  loader.load(
+    leftThighSTL,
+    function (geometry) {
+      geometry.center();
+      const mesh = new THREE.Mesh( geometry, material );
+      mesh.castShadow = true;
+      mesh.receiveShadow = true;
+
+      geometry.translate( 0, 20, -215 );
+      mesh.position.set( -10, 80, -10 );
+      mesh.scale.set( 1, 1, 1 );
+      leftThigh = mesh;
+      hip.add(leftThigh);
+    }
+  )
+}
+
+
+
+
+
+
+// helper functions
+//scene.add(new THREE.AxesHelper(3.5));
+
+// Rendering and animate at set fps
+let clock = new THREE.Clock();
+let delta = 0;
+let interval = 1 / 60;
+function animate() {
+  requestAnimationFrame(animate);
+  delta += clock.getDelta();
+  if (delta  > interval) {
+
+
+
+    if (hip != null && rightThigh != null && leftThigh != null){
+      if (window.currentData != null) animateData();
+      else walkingAnimation();
+      controls.update();
+      renderer.render(scene, camera);
+      delta = delta % interval;
+    }
+
+
+
+  }
+}
+animate();
+
+
+
+// Animate the data
+let q, qh, qr, ql;
+function animateData(){
+  const data = window.currentData;
+  qh = new THREE.Quaternion(data[7], data[8], data[9], data[6]).normalize();
+  hip.rotation.setFromQuaternion(qh);
+  
+  q = new THREE.Quaternion(data[17], -data[18], -data[19], data[16]).normalize();
+  qr = new THREE.Quaternion().multiplyQuaternions (qh,q).normalize();
+  rightThigh.rotation.setFromQuaternion(qr);
+
+  q = new THREE.Quaternion(data[27], -data[28], -data[29], data[26]).normalize();
+  ql = new THREE.Quaternion().multiplyQuaternions (qh,q).normalize();
+  leftThigh.rotation.setFromQuaternion(ql);
+}
+
+
+
+// Making a walking animation
+let counter = 0;
+const speed = 1.1;
+function walkingAnimation(){
+  counter += Math.PI*interval*speed;
+  const sc = Math.sin(counter);
+
+  hip.rotation.z += Math.PI/4*interval*Math.sqrt(speed);
+  hip.rotation.x = 0.08*sc;
+  hip.position.z = 0.2*sc;
+
+  q = new THREE.Quaternion(0.05,0.18,0,sc-0.2);
+  leftThigh.rotation.setFromQuaternion(q);
+  q = new THREE.Quaternion(0.05,0.18,0,-sc-0.2);
+  rightThigh.rotation.setFromQuaternion(q);
+}
+
+
---
+++ js/serial.js
@@ -0,0 +1,153 @@
+let port, writer, currentData;
+
+// Buffer sizes for displaying, extracting currentData and saving to csv
+const currentDataBufferLength = 1000;
+
+
+
+
+
+// Default baud value
+document.getElementById("baud").value = (localStorage.baud == undefined ? 500000 : localStorage.baud);
+
+
+
+
+
+// sleep function
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+
+
+
+// Attempt to connect to serial port
+window.connectSerial = async function connectSerial() {
+    try {
+        // Prompt user to select any serial port.
+        port = await navigator.serial.requestPort();
+        await port.open({ baudRate: document.getElementById("baud").value });
+        listenToPort();
+
+        const textEncoder = new TextEncoderStream();
+        const writableStreamClosed = textEncoder.readable.pipeTo(port.writable);
+
+        writer = textEncoder.writable.getWriter();
+    } catch {
+        alert("Serial Connection Failed");
+    }
+}
+
+
+
+
+
+// Send data via serial port
+window.sendSerialLine = async function sendSerialLine() {
+    dataToSend = document.getElementById("lineToSend").value;
+    if (document.getElementById("addLine").checked == true) dataToSend = dataToSend + "\r\n";
+    if (document.getElementById("echoOn").checked == true) appendToTerminal("> " + dataToSend);
+    await writer.write(dataToSend);
+    document.getElementById("lineToSend").value = "";
+    //await writer.releaseLock();
+}
+
+
+
+
+
+// Catching data from port
+async function listenToPort() {
+    sleep(100);
+    const textDecoder = new TextDecoderStream();
+    const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
+    const reader = textDecoder.readable.getReader();
+
+    // Listen to data coming from the serial device.
+    try {
+        while (true) {
+            const { value, done } = await reader.read();
+            // value is a string.
+            if (value != "\n" && value != null) { 
+                updateCsvBuffer(value);
+                getCurrentData();
+                
+            }
+        }
+    } catch {
+    } finally {reader.releaseLock();}
+}
+
+
+
+
+
+// Limit buffer size while removes beginning lines that are incomplete
+function limitLines(buffer, maxLength) {
+    if (buffer.length > maxLength){
+        buffer = buffer.slice(buffer.length - maxLength);
+        const pos = buffer.match(/\n/).index + 1;
+        buffer = buffer.slice(pos);
+    }
+    return buffer;
+}
+
+
+
+
+
+// Get latest full line of data by checking through the latest datachunk
+function getCurrentData() {
+    if (csvBuffer.length > currentDataBufferLength){
+        let currentDataBuffer = limitLines(csvBuffer, currentDataBufferLength);
+        currentDataBuffer = currentDataBuffer.split("\n");
+        // Extract latest line of data and split into array
+        try {
+        currentData = currentDataBuffer[currentDataBuffer.length-2].split(',').map(Number);
+        window.currentData = currentData;
+        } catch {}
+    }
+}
+
+
+
+
+
+// Buffer for saving CSV file
+let csvBufferSize = 30*100*10*10;
+let csvBuffer = '';
+document.getElementById("csvBufferSize").value = csvBufferSize;
+function updateCsvBuffer(newStuff) {
+    csvBuffer += newStuff;
+    csvBuffer = limitLines(csvBuffer, csvBufferSize);
+}
+
+
+
+
+
+window.clearBuffers = function clearBuffers(){
+    csvBuffer = '';
+    csvBuffer = '';
+}
+
+
+
+
+
+// Saving data as CSV
+let csvName = "test";
+window.saveCSV = function saveCSV(){
+    csvName = window.prompt("Enter file name: ", csvName);
+    /*/
+    const now = new Date();
+    filename += " " + now.getDate() + "-" + now.getMonth() + "-" + now.getFullYear();
+    filename += ", " + now.getUTCHours() + "-" + now.getUTCMinutes() + "-" + now.getUTCSeconds();
+    //*/
+    var pom = document.createElement('a');
+    pom.setAttribute('href', 'data:text/csv;charset=utf-8,'+csvBuffer);
+    pom.setAttribute('download', csvName);
+    pom.click();
+};
